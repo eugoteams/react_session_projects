b30d66a26f25938524d87fabd62153c8e3669bad
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2023-12-19 00:57:18 +0530

    Select Component Project

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import Calculator from "./Porject_03_Calculator_App";
 import CounterApp from "./Project_01_Counter_App";
 import FormValidation from "./Project_02_FormValidation_App";
 import TicTacToe from "./Project_04_Tic_Tac_Toe_App";
+import SelectComponent from "./Project_05_Select_Compoenent_App";
 
 function App() {
   return (
     <div className="App">
       {/* <CounterApp /> */}
-      <TicTacToe />
+      {/* <TicTacToe /> */}
       {/* <FormValidation /> */}
       {/* <Calculator /> */}
+      <SelectComponent
+        name={"your favorite library"}
+        placeholder={"select your favorite library"}
+        data={["react", "angular", "vue", "svelte"]}
+      />
     </div>
   );
 }
---
+++ src/Project_05_Select_Compoenent_App/index.js
@@ -0,0 +1,59 @@
+/** @format */
+
+import React, { Fragment, useState } from "react";
+import style from "./Select.module.css";
+
+const SelectComponent = ({ name, placeholder, data }) => {
+  const [value, setSelectedValue] = useState("");
+  const [dropDown, setDropDown] = useState(false);
+
+  const onClickListener = (e) => {
+    setDropDown((prevState) => !prevState);
+  };
+
+  const onItemSelected = (e) => {
+    console.log(e.target.innerText);
+    setSelectedValue((prevState) => e.target.innerText);
+    onClickListener();
+  };
+
+  console.log(value);
+  return (
+    <Fragment>
+      <div className={`${style.container}`}>
+        <h2> Select Component</h2>
+        <div className={`${style.input_field}`}>
+          <label>{name}</label>
+          <input
+            type="text"
+            placeholder={placeholder}
+            onClick={onClickListener}
+            value={value}
+            onChange={(e) => {}}
+          />
+        </div>
+        {dropDown && (
+          <div className={`${style.dropdown}`}>
+            {data.map((item, index) => {
+              return (
+                <p
+                  key={`drop_down_${index}`}
+                  className={
+                    value.toLowerCase() === item
+                      ? ` ${style.item} ${style.item_selected}`
+                      : `${style.item}`
+                  }
+                  onClick={onItemSelected}
+                >
+                  {item}
+                </p>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </Fragment>
+  );
+};
+
+export default SelectComponent;
